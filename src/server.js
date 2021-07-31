ab5846eaa6420d7aa9a3a9377157155f98c3d8ee
faisal-t <faizaltampanzz@gmail.com>
2021-07-31 15:52:09 +0700

    31-07-2021

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 /* eslint-disable quotes */
 /* eslint-disable eol-last */
 
+// konfigurasi dotenv
 require('dotenv').config();
+
+// konfigurasi hapi
 const Hapi = require("@hapi/hapi");
+
+// konfigurasi notes
 const notes = require("./api/notes");
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require("./validator/notes");
 
+// konfigurasi users
+const users = require("./api/notes");
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require("./validator/users");
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -24,6 +36,11 @@ const init = async () => {
     options: {
       service: notesService,
       validator: NotesValidator,
+    },
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
     },
   });
 
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -53,6 +53,28 @@ class UsersService {
       throw new NotFoundError('User tidak ditemukan');
     }
   }
+
+  async verifyUserCradential(username, password){
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+ 
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+ 
+    const { id, password: hashedPassword } = result.rows[0];
+ 
+    const match = await bcrypt.compare(password, hashedPassword);
+ 
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
+  }
 }
 
 module.exports = UsersService;
